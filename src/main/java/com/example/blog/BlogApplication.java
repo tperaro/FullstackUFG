500b096ac8bc6c59802c16e961dfd1d5a78c6b86
Carlos Felipe <carlos@carlosfelipe.dev>
2023-02-11 19:06:27 -0300

    Altera JDK para 17; Altera Spring para 2.7; Configura SpringFox

--- src/main/java/com/example/blog/BlogApplication.java
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
